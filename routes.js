e67289f512e99df00cb1da3f76568e4deb671c9d
Vinicius1441 <60555584+Vinicius1441@users.noreply.github.com>
2020-12-04 16:25:33 -0300

    feature: create instructor front-end

--- routes.js
+++ routes.js
@@ -5,12 +5,21 @@ const routes = express.Router();
 routes.get("/", (req, res) => {
   return res.redirect("/instructors");
 });
+
 routes.get("/instructors", (req, res) => {
   return res.render("instructors/index");
 });
 
+routes.get("/instructors/create", (req, res) => {
+  return res.render("instructors/create");
+});
+
 routes.get("/members", (req, res) => {
   return res.render("members");
+});
+
+routes.post("/instructors", (req, res) => {
+  return res.send("recebido");
 });
 
 module.exports = routes;
