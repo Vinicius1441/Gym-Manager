44608272aff7990fba08d34dd8601ba58d530fcc
Vinicius1441 <60555584+Vinicius1441@users.noreply.github.com>
2021-01-22 12:40:04 -0300

    feature: edting and deleting

--- instructors.js
+++ instructors.js
@@ -78,3 +78,47 @@ exports.edit = (req, res) => {
 
   return res.render("instructors/edit", { instructor });
 };
+
+exports.put = (req, res) => {
+  const { id } = req.body;
+  console.log(req.body);
+  let index = 0;
+
+  const foundInstructor = data.instructors.find(function (
+    instructor,
+    foundIndex
+  ) {
+    if (id == instructor.id) {
+      index = foundIndex;
+      return true;
+    }
+  });
+
+  const instructor = {
+    ...foundInstructor,
+    ...req.body,
+    birth: Date.parse(req.body.birth),
+  };
+
+  data.instructors[index] = instructor;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("write file error!");
+    return res.redirect(`/instructors/${id}`);
+  });
+};
+
+exports.delete = (req, res) => {
+  const { id } = req.body;
+
+  const filteredInstructors = data.instructors.filter((instructor) => {
+    return instructor.id != id;
+  });
+
+  data.instructors = filteredInstructors;
+
+  fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
+    if (err) return res.send("Write file error");
+    res.redirect("/instructors");
+  });
+};
